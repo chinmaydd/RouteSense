e5b1e01cf7b8fec58027833c69097b833d1301d5
chinmaydd <chinmay1dd@gmail.com>
2016-11-06 18:39:27 +0530

    Fixes setup phase issues

--- MCFAlgorithm.py
+++ MCFAlgorithm.py
@@ -1,126 +1,65 @@
-###########################
-# Minimum Cost Forwarding #
-# Author: chinmay_dd      #
-###########################
-
 # Using the local development version of pymote
 import sys
 sys.path.append("/home/chinmay_dd/Projects/ACN.WSN/pymote2.0")
 from pymote.message import Message
 from pymote.algorithm import NodeAlgorithm
 import pdb
+from pymote.logger import logger
 # Using infinity from the numpy module
 # Although, we will be using a infinity-like value in the actual implementation,
 # this particular logic helps in algorithm implementation.
 from numpy import inf
 from math import isinf
 
-# MCF Algorithm implementation (along with optimization)
-class MCF(NodeAlgorithm):
-  # Key for data being updated in the node.
-  required_params = ('sinkKey',)
-  default_params = {}
+# Some premise for application of this algorithm.
+# The minimum cost field must already be setup.
+# A node should be chosen who wants to transmit data to another.
+# It's status should be "TRANSMIT"
+# It's memory should contain the destination node as "DEST"
+class MCFAlgorithm(NodeAlgorithm):
+    required_params = ('transmitNode',)
+    default_params = {}
+    destination = None
 
-# Helper method to check if the message is an advertisement
-  def check_if_adv(self, message):
-    return (message.header == self.ADV or message.header == NodeAlgorithm.INI)
+    TRA = "TRANSMIT"
 
-  # Header used to advertise the lowest cost.
-  ADV = "advertisement"
+    def initializer(self):
+        for node in self.network.nodes():
+            if node.status == "TRANSMIT":
+                cost_string = "MCost=" + str(node.memory["BCost"])
+                node.send(Message(header=self.TRA, data=cost_string))
+                node.status = "DONE"
+            elif node.id != 1: # Hacky, but works for now
+                node.status = "IDLE"
 
-  # Status initializer for each node.
-  def initializer(self):
-    for node in self.network.nodes():
-      node.memory["Timer"] = inf
-      # If the node is a sink node, broadcase the message and set it to done.
-      if self.sinkKey in node.memory:
-        self.network.outbox.insert(0, Message(header=NodeAlgorithm.INI,
-          source=node, data=node.memory[self.sinkKey]))
+    def link_cost(self, message):
+        # Using SNR for this.
+        signal_to_noise_ratio = message.destination.snr[0]
+        l_cost = (0-(signal_to_noise_ratio/10))
+
+        return l_cost
+
+    def idle(self, node, message):
+        BCost = float(message.data.split("=")[1])
+        mlink_cost = self.link_cost(message)
+        
+        remaining_budget = BCost - mlink_cost
+
+        if float(node.memory["BCost"]) <= remaining_budget:
+            cost_string = "MCost="+str(node.memory["BCost"])
+            node.send(Message(header=self.TRA, data=cost_string))
+          
         node.status = "DONE"
-      else:
-        # Set all other nodes to "IDLE"
-        node.status = "IDLE"
-        node.memory["BCost"] = inf
-  
-  # Method to calculate link cost from A->B
-  # Cost of the link maybe a composite of:
-  # hop count
-  # consumed wireless energy
-  # delay between source and sink
-  # function of received signal strength (we can use the snr component, 
-  # i.e signal to noise ratio
-  # number of retransmissions
-  def calculate_cost(self, message):
-    # Energy consumed during receving the message
-    # consumed_wireless_energy = message.destination.power.energy_consumption
-    # Closer the SNR is to 0 ==> Better the signal received
-    # SNR contains all the params. We can choose it to calculate link_cost
-    signal_to_noise_ratio = message.destination.snr[0]
-    # Normalizing the snr value as (0-snr/10) and adding the already existing
-    # message cost(received from the message) to the same.
-    link_cost = float(message.data) + (0-(signal_to_noise_ratio/10))
-    
-    return link_cost
-  
-  # After the initialization process, each node is set to an idle state.
-  # On receiving the first "ADV", the node should transition itself to the
-  # listening state. 
-  # This implies that further messages are received but not broadcasted.
-  # The broadcast duration will depend on a "backoff" algorithm as mentioned in
-  # the reference paper.
-  def idle(self, node, message):
-    # Now, depending on the header of the message we can make two choices:
-    # If the header is NodeAlgorithm.INI, this implies that the node is a
-    # neighbor to the sink node.
-    # Otherwise, the node is just another node in the network.
-    # Either way, we will have the node transition into the "LISTENING" state
-    # and implement the backoff algorithm.
-    if self.check_if_adv(message):
-      message_cost = self.calculate_cost(message)
-      node.memory["BCost"] = message_cost
-      node.memory["Timer"] = int(message_cost/2)
-      node.status = "LISTENING"
-    else:
-      # This implies that the algorithm is now in operational mode.
-      pass
 
-  def listening(self, node, message):
-    pass
+    def done(self, node, message):
+        pass
 
-  def done(self, node, message):
-    pass
+    def wait(self, node, message):
+        if message.header == self.TRA:
+          logger.info("Message to sink received!")
 
-  def step(self, node):
-    # Standard step code
-    message = node.receive()
-    if message:
-      if (message.destination == None or message.destination == node):
-        # when destination is None it is broadcast message
-        return self._process_message(node, message)
-      elif (message.nexthop == node.id):
-        self._forward_message(node, message)
-
-    # Timer-like code.
-    # pdb.set_trace()
-    if message and node.status == "LISTENING":
-      if self.check_if_adv(message):
-        message_cost = self.calculate_cost(message)
-        # Check if the advertised cost is less than the existing.
-        if message_cost < node.memory["BCost"]:
-          # Set the new cost.
-          node.memory["BCost"] = message_cost
-          # Restart the timer.
-          node.memory["Timer"] = int(message_cost/2)
-    elif not isinf(node.memory["Timer"]) and node.status == "LISTENING":
-      # pdb.set_trace()
-      node.memory["Timer"] -= 1
-      if node.memory["Timer"] == 0:
-        # Broadcast the new message and set the status as done.
-        node.send(Message(header=self.ADV, data=str(node.memory["BCost"])))
-        node.status = "DONE"
- 
-  STATUS = {
-      "IDLE": idle,
-      "DONE": done,
-      "LISTENING": listening
-      }
+    STATUS = {
+            "WAIT": wait,
+            "IDLE": idle,
+            "DONE": done,
+            }
--- first_sim.py
+++ first_sim.py
@@ -1,13 +1,16 @@
 # Using the local development version of pymote
 import sys
+import MCFSetup
 import MCFAlgorithm
 import pdb
 sys.path.append("/home/chinmay_dd/Projects/ACN.WSN/pymote2.0")
+from pymote.algorithm import Algorithm
 from pymote import *
 
-net_gen = NetworkGenerator(5, 5, 5)
+# Let's work with 10 nodes.
+net_gen = NetworkGenerator(10,10,10)
 net = net_gen.generate_random_network()
-net.algorithms = ((MCFAlgorithm.MCF, {"sinkKey":"ICost"}),)
+net.algorithms = ((MCFSetup.MCFSetup, {"sinkKey":"ICost"}),)
 
 # Setting the 0th node to be the base.
 first_node = net.nodes()[0]
@@ -17,11 +20,23 @@ first_node.type = "B"
 
 sim = Simulation(net)
 sim.run()
+
+net.algorithms = ((MCFSetup.MCFSetup, {"sinkKey":"ICost"}), (MCFAlgorithm.MCFAlgorithm, {"transmitNode": "T"}),)
+
+# Choosing random numbers 3 and 9
+last_node = net.nodes()[9]
+# Something
+last_node.memory["T"] = "S"
+last_node.status = "TRANSMIT"
+
+# Set the status as wait.
+first_node.status = "WAIT"
+
+sim = Simulation(net)
+sim.run()
+
+print [x.memory["BCost"] for x in net.nodes()]
+
 net.show()
 
 pdb.set_trace()
-
-# for node in net.nodes():
-#     print node.id
-#     print node.memory["BCost"]
-#     print "-------"
